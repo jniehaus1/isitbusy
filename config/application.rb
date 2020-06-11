24c10d8e215aeff3b356af457714c70c99bb56ac
jniehaus1 <jniehaus@ziprecruiter.com>
2020-06-11 15:52:50 -0400

    No database mode

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require 'action_controller/railtie'
+require 'sprockets/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
